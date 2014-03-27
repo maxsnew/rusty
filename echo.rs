632d6eda50a6ff21ce18792b0067457f483e9ca0
Max New <maxsnew@gmail.com>
2014-03-27 16:41:47 -0500

    Fully implement echo

--- echo.rs
+++ echo.rs
@@ -1,8 +1,22 @@
 fn main() {
     let args = std::os::args();
 
-    for i in range(1, args.len()) {
-        print(format!("{:s} ", args[i]));
+    let t = 
+        if args.len() > 1 && args[1] == ~"-n" {
+            (2, false)
+        } else {
+            (1, true)
+        };
+    
+    match t {
+        (start, print_newline) => {
+            for i in range(start, args.len()) {
+                print(args[i]);
+                print(" ");
+            };
+            if print_newline {
+                print("\n");
+            }
+        }
     }
-    println("");
 }

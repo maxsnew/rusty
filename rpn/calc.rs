72fda7b0dcf1d69cf1ec62baad3fa5ec34b5ebd9
Max New <maxsnew@gmail.com>
2014-03-28 18:52:31 -0500

    Better rpn

--- rpn/calc.rs
+++ rpn/calc.rs
@@ -2,12 +2,28 @@ extern mod extra;
 
 use std::io::buffered::BufferedReader;
 use std::io::stdin;
-use std::vec::append;
 
 #[deriving(Clone, Eq)]
 enum Tok { 
     Num(int),
-    Plus
+    Oper(Op)
+}
+
+#[deriving(Clone, Eq)]
+enum Op {
+    Plus,
+    Minus,
+    Times
+}
+
+impl Op {
+    fn interp(self, x: int, y: int) -> int {
+        match self {
+            Plus  => x+y,
+            Minus => x-y,
+            Times => x*y
+        }
+    }
 }
 
 #[deriving(Clone, Eq)]
@@ -25,29 +41,27 @@ impl Stack {
 fn parse(s: ~str) -> Option<~[Tok]> {
     let mut words = ~[];
     for word in s.words(){
-        if word == "+" {
-            words.push(Plus)
-        } else {
-            match from_str(word) {
-                None    => return None,
-                Some(n) => words.push(Num(n))
-            }
+        match word {
+            "+" => words.push(Oper(Plus)),
+            "-" => words.push(Oper(Minus)),
+            "*" => words.push(Oper(Times)),
+            _   => 
+                match from_str(word) {
+                    None    => return None,
+                    Some(n) => words.push(Num(n))
+                }
         }
     }
     Some(words)
-}
-
-fn step(tok: Tok, toks: &[Tok]) -> Option<~[Tok]> {
-    None
 }
 
 fn run_rpn(toks: &[Tok]) -> Option<int> {
     let mut stack = ~Empty;
     for tok in toks.iter() {
         match (*tok, *stack) {
-            (Num(n), o) => stack = o.push(*tok),
-            (Plus, Cons(Num(x), ~Cons(Num(y), rest))) =>
-                stack = ~Cons(Num(x+y), rest),
+            (Num(_), o) => stack = o.push(*tok),
+            (Oper(o), Cons(Num(x), ~Cons(Num(y), rest))) =>
+                stack = ~Cons(Num(o.interp(x,y)), rest),
             _ => return None
         }
     }
@@ -77,8 +91,10 @@ fn main() {
 fn run_rpn_test() {
     assert!(run_rpn([]) == None);
     assert!(run_rpn([Num(1)]) == Some(1));
-    assert!(run_rpn([Plus]) == None);
+    assert!(run_rpn([Oper(Plus)]) == None);
     assert!(run_rpn([Num(1),Num(2)]) == None);
-    assert!(run_rpn([Num(1), Num(2), Plus]) == Some(3));
-    assert!(run_rpn([Num(1), Num(2), Plus, Num(7), Plus]) == Some(10));
+    assert!(run_rpn([Num(1), Num(2), Oper(Plus)]) == Some(3));
+    assert!(run_rpn([Num(4), Num(47), Oper(Minus)]) == Some(43));
+    assert!(run_rpn([Num(7), Num(2), Oper(Times)]) == Some(14));
+    assert!(run_rpn([Num(1), Num(2), Oper(Plus), Num(7), Oper(Times)]) == Some(21));
 }
